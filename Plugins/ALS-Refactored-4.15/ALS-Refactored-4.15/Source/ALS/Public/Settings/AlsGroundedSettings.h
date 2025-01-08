856b4b52d8baa82f8171fbeee77c2c21969a7f47
Wendy <87863350+jt831@users.noreply.github.com>
2025-01-08 08:48:50 +0800

    Refactor

---
+++ Plugins/ALS-Refactored-4.15/ALS-Refactored-4.15/Source/ALS/Public/Settings/AlsGroundedSettings.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "AlsGroundedSettings.generated.h"
+
+class UCurveFloat;
+
+USTRUCT(BlueprintType)
+struct ALS_API FAlsGroundedSettings
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS")
+	TObjectPtr<UCurveFloat> RotationYawOffsetForwardCurve;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS")
+	TObjectPtr<UCurveFloat> RotationYawOffsetBackwardCurve;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS")
+	TObjectPtr<UCurveFloat> RotationYawOffsetLeftCurve;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS")
+	TObjectPtr<UCurveFloat> RotationYawOffsetRightCurve;
+
+	// The higher the value, the faster the interpolation. A zero value results in instant interpolation.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ALS", Meta = (ClampMin = 0))
+	float VelocityBlendInterpolationSpeed{12.0f};
+};
